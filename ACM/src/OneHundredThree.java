bd6e1b86f3a265f6230469b51307370f8f33a4a7
Roy Zheng <roycraft3@gmail.com>
2014-04-11 16:46:56 -0700

    103

---
+++ ACM/src/OneHundredThree.java
@@ -0,0 +1,78 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author roycr_000
+ */
+public class OneHundredThree {
+
+    class Box {
+
+        int number;
+        int[] dimensions;
+
+        public Box(int number, int[] dimensions) {
+            this.number = number;
+            this.dimensions = dimensions;
+        }
+    }
+
+    Map<Box, Integer> nest;
+    List<Box> boxes;
+
+    public OneHundredThree() {
+
+    }
+
+    private void reset() {
+        this.nest = new HashMap<>();
+        this.boxes = new ArrayList<>();
+    }
+
+    public void run(Scanner in) {
+        while (in.hasNextInt()) {
+            this.reset();
+            int numBoxes = in.nextInt();
+            int numDim = in.nextInt();
+            for (int boxNum = 0; boxNum != numBoxes; boxNum++) {
+                int[] dim = new int[numDim];
+                for (int i = 0; i != numDim; i++) {
+                    dim[i] = in.nextInt();
+                }
+                boxes.add(new Box(boxNum, dim));
+            }
+            Box solution = this.solveOne();
+            System.out.println(this.nest.get(solution));
+        }
+    }
+
+    private Box solveOne() {
+        this.sortBoxes();
+        for (Box box : this.boxes) {
+            this.calculateBoxNest(box);
+        }
+        return this.findMax(this.nest);
+    }
+
+    private void sortBoxes() {
+
+    }
+
+    private void calculateBoxNest(Box b) {
+
+    }
+
+    private Box findMax(Map<Box, Integer> nests) {
+        return null;
+    }
+}
