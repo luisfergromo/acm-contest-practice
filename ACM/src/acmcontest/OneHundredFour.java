abb0d92ca0a5635a5b7fdc483337af8915a4afc6
Yiwei Zheng <yiweizhe@usc.edu>
2014-04-12 13:27:37 -0700

    104 start

---
+++ ACM/src/acmcontest/OneHundredFour.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acmcontest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author roycr_000
+ */
+public class OneHundredFour {
+
+    class Currency {
+
+        Map<Currency, Double> conversion = new HashMap<Currency, Double>();
+
+        Currency() {
+
+        }
+
+    }
+
+    OneHundredFour() {
+
+    }
+
+    public void run(Scanner in) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
